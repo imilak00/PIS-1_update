026e932abb75aeb0ddc8ef6a191fa25a20df2fbe
imilak00 <imilak00@fesb.hr>
2019-06-11 15:45:15 +0200

    afia

--- newClass.cs
+++ newClass.cs
@@ -9,15 +9,11 @@ namespace ConsoleApplication1
     
     class newClass
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         void newMethod()
         {}
-=======
-        "Console.WriteLine("muhaha");
->>>>>>> 8d3d057778573299be788cce6f16a1430c44d863
-=======
-        
->>>>>>> 1fa2dd91e3c98a434c7156480e0f470f6d8966ff
+
+       
+
     }
 }
